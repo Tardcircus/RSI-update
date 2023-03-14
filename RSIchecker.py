5dea9735e1595727a7cdaa92e60bd961d560eb72
Tardcircus <106075953+Tardcircus@users.noreply.github.com>
2023-03-14 14:29:00 -0700

    Create RSIchecker.py

---
+++ RSIchecker.py
@@ -0,0 +1,91 @@
+import time
+import requests
+import smtplib, ssl
+
+
+#
+#  Replace inside od Quotes. dont add quotes to port number
+#
+
+Server_host = "YOUR EMAIL PROVIDER HOST"
+sender_email = "SENDER_EMAIL"
+receiver_email = "RECIEVER_EMAIL"
+port = EMAIL_PORT
+app_password = "YOUR EMAIL PASSWORD"
+
+
+# URL of the website to monitor
+url = "https://status.robertsspaceindustries.com/"
+url2 = "https://robertsspaceindustries.com/patch-notes"
+
+
+prev_content = " "
+
+while True:
+    try:
+        # Fetch the website content
+        response = requests.get(url)
+        content = response.text
+    except requests.exceptions.RequestException as e:
+        print(e)
+        continue
+
+    # Compare the content with the previous content
+    if content != prev_content:
+
+        message = """\
+        From: Star.Citizen.Update@NotRSI.com
+        Subject: STAR CITIZEN SERVER STATUS CHANGE
+
+        check https://status.robertsspaceindustries.com/ for current status change
+
+        """
+
+        context = ssl.create_default_context()
+
+
+
+        with smtplib.SMTP_SSL(Server_host, port, context=context) as server:
+            server.login("YOUR_EMAIL", app_password)
+            server.sendmail(sender_email, receiver_email, message)
+
+
+    # Update the previous content
+    prev_content = content
+
+
+# Patch patch-notes
+
+prev_content2 = " "
+
+    try:
+        # Fetch the website content
+        response = requests.get(url2)
+        content = response.text
+    except requests.exceptions.RequestException as e:
+        print(e)
+        continue
+
+    # Compare the content with the previous content
+    if content2 != prev_content2:
+        message2 = """\
+        From: Star.Citizen.Update@NotRSI.com
+        Subject: STAR CITIZEN SERVER PATCH UPDATE
+
+        check https://robertsspaceindustries.com/patch-notes for current patch update
+
+        """
+        context = ssl.create_default_context()
+
+
+        with smtplib.SMTP_SSL("Server_host", port, context=context) as server:
+            server.login("YOUR_EMAIL", app_password)
+            server.sendmail(sender_email, receiver_email, message2)
+
+
+    # Update the previous content
+    prev_content2 = content2
+
+    # Wait for 60 seconds before checking again
+    time.sleep(60)
+    print("Rechecking")
